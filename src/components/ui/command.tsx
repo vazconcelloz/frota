452cb01931b135e9fa2808a6bcf60900359cf2f7
vazconcelloz <almeidains00@gmail.com>
2026-03-12 16:03:26 -0300

    erros

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -22,6 +22,7 @@ const Command = React.forwardRef<
 Command.displayName = CommandPrimitive.displayName;
 
 interface CommandDialogProps extends DialogProps {}
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 
 const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
